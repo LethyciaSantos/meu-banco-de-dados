45abdcb604fa3769ec18f788188a357e6c81fc60
LethyciaSantos <156119946+LethyciaSantos@users.noreply.github.com>
2024-01-25 17:30:43 -0300

    conectei o banco de dados a api

---
+++ firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC6iDCoPBTbxJKWN22qd5bG0tbl-WsPefw",
+  authDomain: "teste-b006e.firebaseapp.com",
+  projectId: "teste-b006e",
+  storageBucket: "teste-b006e.appspot.com",
+  messagingSenderId: "822779795723",
+  appId: "1:822779795723:web:39e4fbbe8bb18df21bea0e",
+  measurementId: "G-WWVYQQVNK8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
