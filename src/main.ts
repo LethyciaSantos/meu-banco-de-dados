ed533bca5fe026b365c3c892938c5aab418db51a
LethyciaSantos <156119946+LethyciaSantos@users.noreply.github.com>
2024-01-31 17:41:36 -0300

    criação de rota

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import {db, firestore} from '../banco de dados/firebase';
 
 const app = express(); 
 
+app.get('/', (reg, res) => {
+    res.send("bem vindo a minha primeira API");
+})
+app.post('/usuario',async (reg, res) => {
+    const usuario = req.body.nome
+    try{
+        const docRef = await firestore.addDoc(firestore.collection(db, ususario),{
+       nome: nome,     
+        }) res.send(docRef.id)
+    } catch (e) {
+        console.log(e)
+        res.status(500.send(e))
+    }
+})
 app.listen(3000, function(){
     console.log("servidor rodando na porta http://localhost:3000")
 });
