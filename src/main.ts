1dd34deb7884093fb91825d5a5b450f61d74d800
LethyciaSantos <156119946+LethyciaSantos@users.noreply.github.com>
2024-02-21 17:00:59 -0300

    adicionei cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { db, firestore } from '../banco de dados/firebase';
+import cors from "cors"
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+}))
 
 app.get('/', (reg, res) => {
     res.send("bem vindo a minha primeira API");
